bbd518a2b1606a68e87e37b3393866ade8016d04
scowart66 <cowart_66@live.com>
2015-09-18 22:32:04 -0400

    Initial project

---
+++ Unity2DPlatformer/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+	public Vector2 velocity = new Vector2(5,0);
+	public float distance = 5;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = velocity * transform.localScale.x;
+		distance += Mathf.Abs (transform.position.x);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Mathf.Abs (transform.position.x) > distance)
+			Destroy (gameObject);
+	}
+}
---
+++ Unity2DPlatformer/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float speed = 10f;
+	public Vector2 maxVelocity = new Vector2(3,5);
+	public float jumpSpeed = 5f;
+	public bool standing;
+	public GameObject ammo;
+	public Transform fireMarker;
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var absVelX = Mathf.Abs (GetComponent<Rigidbody2D> ().velocity.x);
+		var absVelY = Mathf.Abs (GetComponent<Rigidbody2D> ().velocity.y);
+		if (absVelY <= 0.5f) {
+			standing = true;
+		} else {
+			standing = false;
+		}
+		var forceX = 0f;
+		var forceY = 0f;
+
+		if (Input.GetKey ("right")) {
+			if (absVelX < maxVelocity.x)
+				forceX = speed;
+			this.transform.localScale = new Vector3 (1, 1, 1);
+			animator.SetInteger ("AnimState", 1);
+		} else if (Input.GetKey ("left")) {
+			if (absVelX < maxVelocity.x)
+				forceX = -speed;
+			this.transform.localScale = new Vector3 (-1, 1, 1);
+			animator.SetInteger ("AnimState", 1);
+		} else {
+			animator.SetInteger("AnimState", 0);
+		}
+		if (Input.GetKeyDown ("up") && standing) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpSpeed);
+		}
+		if (Input.GetKey ("space") && standing) {
+			animator.SetInteger ("AnimState", 2);
+			GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+		} else {
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (forceX, forceY));
+		}
+	}
+
+	public void Fire(){
+		if (ammo != null) {
+			var clone = Instantiate (ammo, fireMarker.position, Quaternion.identity) as GameObject;
+			clone.transform.localScale = transform.localScale;
+		}
+	}
+}
